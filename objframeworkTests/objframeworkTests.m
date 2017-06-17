e6815764dd0bc8d34291f4b61240ab7e2077b4d8
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-06-17 22:34:05 +0900

    Initial Commit

---
+++ objframeworkTests/objframeworkTests.m
@@ -0,0 +1,39 @@
+//
+//  objframeworkTests.m
+//  objframeworkTests
+//
+//  Created by 水野祥子 on 2017/06/17.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface objframeworkTests : XCTestCase
+
+@end
+
+@implementation objframeworkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
