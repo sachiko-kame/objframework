113099cfe853a7930ba93d90ba8c7de930b046ea
sachiko-kame <taiyousannsann2525sann@yahoo.co.jp>
2017-06-17 22:34:58 +0900

    ファイル作成
    cocoatuchclassでhとm

---
+++ objframework/sample1.m
@@ -0,0 +1,13 @@
+//
+//  sample1.m
+//  objframework
+//
+//  Created by 水野祥子 on 2017/06/17.
+//  Copyright © 2017年 sachiko. All rights reserved.
+//
+
+#import "sample1.h"
+
+@implementation sample1
+
+@end
